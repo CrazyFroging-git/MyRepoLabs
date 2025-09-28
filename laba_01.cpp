301d5d7b26df11bdcc52972e8c66bd8ce0ac0878
CrazyFroging-git <danuypletosy377@gmail.com>
2025-09-28 17:18:57 +0200

    123

--- laba_01.cpp
+++ laba_01.cpp
@@ -2,11 +2,14 @@
 
 
 int main() {
+    std::cout << "unsigned short 16 min-0 max-" << pow(2, 16) - 1 << "\n";
+    std::cout << "int 32 min-" << (pow(2, 31)) << "max-" << pow(2, 31) - 1 << "\n";
     unsigned short first_number; //unsigned, потому что нам дают неотрицательное число, следовательно отрицательные значения не будут поступать, а short потому что нет ограничения на ввод чисел нет 
     unsigned short second_number;
-    unsigned int result_of_prod; // unsigned int потому что два ансайгнед шорт могут 
-    std::cout << "unsigned short "<< "16 " << "min-0 " << "max-" << pow(2, 16) - 1;
     std::cin >> first_number >> second_number;
+    unsigned int result_prod = first_number * second_number; // unsigned int потому что два ансайгнед шорт в умножении поместятся сюда
+    std::cout << result_prod;
+    //int result_sum = 
     
     return 0;
 } 
