f1be84d53d12a9c2dd4e3ce9cb93836cd1c8d08f
CrazyFroging-git <danuypletosy377@gmail.com>
2025-09-28 16:29:43 +0200

    Update laba_01.cpp

--- laba_01.cpp
+++ laba_01.cpp
@@ -1,13 +1,12 @@
 #include <iostream>
 
-// Показать про русский язык
-/*
-111
-222
-333
-*/
 
 int main() {
-    std::cout << 33 << std::endl;
+    unsigned short first_number; //unsigned, потому что нам дают неотрицательное число, следовательно отрицательные значения не будут поступать, а short потому что нет ограничения на ввод чисел нет 
+    unsigned short second_number;
+    unsigned int result_of_prod; // unsigned int потому что два ансайгнед шорт могут 
+    std::cout << "unsigned short "<< "16 " << "min-0 " << "max-" << pow(2, 16) - 1;
+    std::cin >> first_number >> second_number;
+    
     return 0;
-}
+} 
