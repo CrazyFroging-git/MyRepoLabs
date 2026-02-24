8b83d62125a4c0553c94ebcd03621f1d031fc7d2
CrazyFroging-git <danuypletosy377@gmail.com>
2026-02-24 22:37:38 +0200

    библ

---
+++ Семестр 2/laba3/app/main.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <functions.hpp>
+
+//1
+int main() {
+    srand(time(0));
+    setlocale(LC_ALL, "ru_RU.UTF-8");
+    try {
+        std::vector<std::string> items_player1 = {"sword", "shield", "potion"};
+        std::vector<std::string> items_player2 = {"bow", "lom", "skotch", "arrows", "potion"};
+        //startingItems.insert(startingItems.end(), "меч");
+        //std::cout << startingItems.size() << std::endl;
+        Player player1("Иван", 0, 0, 100, "Убегает от монстра", items_player1);
+        //std::cout << player1.GetName() << std::endl;
+        //std::cout << player1.GetPosX() << std::endl;
+        //player1.setPosX(100);
+        Player player2("Роберт", 11, 79, 91, "Обыскивает сундук", items_player2);
+        //std::cout << player1.GetPosX() << std::endl;
+        Player player3 = player1 / player2;
+        player1.ViewAll();
+        //player2.setInventoryRemove("лук");
+        player2.ViewAll();
+        player3.ViewAll();
+    }
+    catch (const std::invalid_argument& e) {
+        std::cout << "Ошибка: " << e.what() << std::endl;
+    }
+    catch (...) {
+        std::cout << "Неизвестная ошибка: " << std::endl;
+    }
+}
---
+++ Семестр 2/laba3/mylib/src/functions.cpp
@@ -0,0 +1,188 @@
+#include <functions.hpp>
+#include <iostream>
+#include <stdexcept>
+#include <algorithm>
+#include <cstdlib>
+
+
+void removeDuplicates(std::vector<std::string>& vec) {    
+    for (size_t i = 0; i < vec.size(); i++) {
+        for (size_t j = i + 1; j < vec.size(); ) {
+            if (vec[j] == vec[i]) {
+                vec.erase(vec.begin() + j);
+            } 
+            else {
+                j++;
+            }
+        }
+    }
+}
+// функция вывода инвентаря
+void InventoryView(std::vector<std::string> inventory_){
+    if (inventory_.empty()) {
+        std::cout << "пусто";
+    } else {
+        // Выводим все предметы через запятую
+        for (size_t i = 0; i < inventory_.size(); ++i) {
+            std::cout << inventory_[i];
+            if (i != inventory_.size() - 1) {
+                std::cout << ", ";
+            }
+        }
+    }
+    std::cout << std::endl;
+}
+
+std::vector<std::string> plus_inventory(std::vector<std::string> result, std::vector<std::string> oth){
+    std::vector<std::string> inventory = result;
+    for (int i = 0; i < oth.size(); i++){
+        if ((std::find(inventory.begin(), inventory.end(), oth[i])) == inventory.end()){
+            inventory.push_back(oth[i]);
+        }
+    }
+    return inventory;
+}
+
+
+
+Player::Player(std::string name, int posx, int posy, int hp, std::string actionstatus, std::vector<std::string> inventory){
+    name_ = name;
+    posx_ = posx;
+    posy_ = posy;
+    hp_ = hp;
+    actionstatus_ = actionstatus;
+    inventory_ = inventory;
+    if (name.empty()) {
+        throw std::invalid_argument("Имя не может быть пустым!");
+    }
+    if (hp < 0) {
+        throw std::invalid_argument("HP должен быть положительным числом!");
+    }
+}
+Player::Player(const Player &other): 
+    name_(other.name_), 
+    posx_(other.posx_), 
+    posy_(other.posy_), 
+    hp_(other.hp_), 
+    actionstatus_(other.actionstatus_),
+    inventory_(other.inventory_){
+    std::cout << "Игрок копирован" << std::endl;
+}
+Player::~Player(){
+    if (!inventory_.empty()){
+        inventory_.clear();
+        std::cout << "Вектор очищен" << std::endl;
+    }
+    
+    std::cout << "-------------Деструктор-------------" << std::endl;
+}
+std::string Player::randomName() {
+    return "Player_" + std::to_string(rand() % 100000);
+}
+Player& Player::operator=(const Player& other){
+    if (this == &other) {
+        return *this;
+    }
+    name_ = other.name_;
+    posx_ = other.posx_;
+    posy_ = other.posy_;
+    hp_ = other.hp_;
+    actionstatus_ = other.actionstatus_;
+    inventory_ = other.inventory_;
+    std::cout << "Игрок присвоен через оператор присваивания" << std::endl;
+    return *this;
+}
+
+Player Player::operator+(const Player& other) const{
+    Player result = *this;
+    removeDuplicates(result.inventory_);
+    result.name_ = result.randomName();
+    result.posx_ = (this->posx_ + other.posx_) / 2;
+    result.posy_ = (this->posy_ + other.posy_) / 2;
+    result.inventory_ = plus_inventory(this->inventory_, other.inventory_);
+    return result;
+}
+Player Player::operator-(const Player& other) const{
+    Player result = *this;
+    result.name_ = result.randomName();
+    if (other.inventory_.empty()) return result;
+    int items_to_remove = rand() % other.inventory_.size() + 1;
+    std::string bufer = other.inventory_[(rand() % other.inventory_.size())];
+    if (std::find(result.inventory_.begin(), result.inventory_.end(), bufer) == result.inventory_.end()){
+        return result;
+    }
+    result.inventory_.erase(std::remove(result.inventory_.begin(), result.inventory_.end(), bufer), result.inventory_.end());
+    return result;
+}
+
+Player Player::operator/(const Player& other) const{
+    Player result = *this;
+    result.name_ = result.randomName();
+    result.posx_ = ((abs(result.posx_) + 1) / (abs(this->posx_) + 1));
+    result.posy_ = ((abs(result.posy_) + 1) / (abs(this->posy_) + 1));
+    int middle1 = result.inventory_.size() / 2;
+    int middle2 = other.inventory_.size() / 2;
+    std::vector<std::string> firstHalf(result.inventory_.begin(), result.inventory_.begin() + middle1);
+    std::vector<std::string> secondHalf(other.inventory_.begin() + middle2, other.inventory_.end());
+    result.inventory_.clear();
+    result.inventory_.reserve(firstHalf.size() + secondHalf.size());
+    result.inventory_.insert(result.inventory_.end(), firstHalf.begin(), firstHalf.end());
+    result.inventory_.insert(result.inventory_.end(), secondHalf.begin(), secondHalf.end());
+    return result;
+}
+
+void Player::setPosX(int posx){
+    posx_ = posx;
+}
+void Player::setPosY(int posy){
+    posy_ = posy;
+}
+void Player::setInventoryInsert(std::string item){
+    inventory_.push_back(item);
+    std::cout << "Элемент -"<< item <<"- добавлен в инвентарь игрока -"<< name_ << "-" << std::endl;
+}
+void Player::setInventoryRemove(std::string item){
+    if (std::find(inventory_.begin(), inventory_.end(), item) == inventory_.end()){
+        std::cout << "Элемента -"<< item <<"- нету в инвентаре игрока -"<< name_ << "-" << std::endl;
+        return;
+    }
+    inventory_.erase(std::remove(inventory_.begin(), inventory_.end(), item), inventory_.end());
+    std::cout << "Элемент -"<< item <<"- убран из инвентаря игрока -"<< name_ << "-" << std::endl;
+}
+void Player::Setactionstatus(std::string actionstatus){
+    actionstatus_ = actionstatus;
+}
+void Player::DrinkFlask(int k){
+    hp_ += k;
+    if (hp_ >= 100){
+        hp_ = 0;
+    }
+}
+void Player::TakeDamage(int k){
+    hp_ -= k;
+    if (hp_ <= 0){
+        hp_ = 0;
+    }
+}
+void Player::ViewInventory() const {
+    std::cout << "Инвентарь игрока " << name_ << ":" << std::endl;
+    for (const auto& item : inventory_) {
+        std::cout << "- " << item << std::endl;
+    }
+    if (inventory_.empty()) {
+        std::cout << "(пусто)" << std::endl;
+    }
+}
+void Player::ViewAll() const{
+    std::cout << "Игрок: " << name_
+      << "     HP: " << hp_ 
+      << "     Позиция: (" << posx_ 
+      << ", " << posy_ << ")"
+      << "     " << "Действие: " << actionstatus_ 
+      << "     "<< "Инвентарь: ";
+      InventoryView(inventory_);
+}
+std::string Player::GetName() const {return name_;}
+int Player::GetPosX() const {return posx_;}
+int Player::GetPosY() const {return posy_;}
+int Player::GetHP() const {return hp_;}
